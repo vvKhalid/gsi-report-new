6e59239ff993186aa0b3da4c127e2f3d5f7d7d9a
vvKhalid <khalid.rzzh2@email.com>
2025-07-22 02:31:20 +0300

    update 3

---
+++ src/app/head.js
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="viewport" content="width=1024, initial-scale=1" />
+      <title>Your Page Title</title>
+      {/* وسوم أخرى */}
+    </>
+  );
+}
