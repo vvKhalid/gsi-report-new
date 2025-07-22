c4a02d469a35ec0e450917dccef9e2f7195f43de
vvKhalid <khalid.rzzh2@email.com>
2025-07-22 19:53:08 +0300

    style6

--- src/app/head.js
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <meta name="viewport" content="width=1024, initial-scale=1" />
-      <title>Your Page Title</title>
-      {/* وسوم أخرى */}
-    </>
-  );
-}
