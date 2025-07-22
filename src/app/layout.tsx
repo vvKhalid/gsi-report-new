8d3ad8e1f0589f0b1d2844c4cb34fca64dde88a5
vvKhalid <khalid.rzzh2@email.com>
2025-07-22 19:32:45 +0300

    style 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=1024" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
